e6dd7533d900b006ee56b19e7eda412723205ea8
Victor-O <mircea.oancea@arnia.ro>
2016-01-14 12:30:33 +0200

    after merge

--- src/app/book/BookController.js
+++ src/app/book/BookController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
   'use strict';
 
@@ -68,29 +67,5 @@
   });
 
 })();
-=======
-/**
- * Created by mihai vict on 08-Jan-16.
- */
-'use strict';
-
-angular.module('app').controller('BookController', function BookController($scope, bookService) {
-  $scope.vm = {};
-  function success(books){
-    console.log(books);
-    $scope.books = books;
-  }
-  function error(response){
-    console.warn(response);
-  }
-  $scope.angularImg = './assets/images/angular.png';
-  $scope.books = [];
-  bookData.getAll()
-    .then(success, error);
-  $scope.selectBook = function (book) {
-    $scope.vm.selectedBook = book;
-  };
-});
->>>>>>> origin/master
 
 
